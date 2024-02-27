83c342be35f3aa7b185336d3e7ab94ff24b97b65
DGimenoX09 <153715113+DGimenoX09@users.noreply.github.com>
2024-02-27 18:09:40 +0100

    Initial commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Enemy : MonoBehaviour
+{
+    private Rigidbody2D rBody;
+
+    private AudioSource source; 
+
+    private BoxCollider2D boxCollider;
+
+    public AudioClip deathSound;
+
+    public Animator animGoomba;
+
+    public float enemySpeed = 5;
+
+    public float enemyDirection = 1;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        animGoomba = GetComponent<Animator>();
+
+        rBody = GetComponent<Rigidbody2D>();
+
+        source = GetComponent<AudioSource>();
+
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rBody.velocity = new Vector2(enemyDirection * enemySpeed, rBody.velocity.y);
+    }
+
+     public void LoadGameOver()
+            {
+            SceneManager.LoadScene("GAMEOVERMENU");
+            }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.layer == 3 || collision.gameObject.tag == "Goombas")
+        {
+                if(enemyDirection == 1)
+            {
+                enemyDirection = -1;
+            }
+            else if (enemyDirection == -1)
+            {
+                enemyDirection = 1;
+            }
+        }
+
+        if(collision.gameObject.tag == "Player")
+        {
+            Destroy(collision.gameObject);
+		    
+            SceneManager.LoadScene("GAMEOVERMENU");
+            
+
+        }
+    }
+
+    public void GoombaDeath()
+    {
+        source.PlayOneShot(deathSound);
+        animGoomba.SetBool("IsDeath", true);
+        boxCollider.enabled = false;
+        rBody.gravityScale = 0;
+        enemyDirection = 0;
+        Destroy(gameObject, 0.5F);
+    }
+}
